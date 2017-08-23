1e97ced1ca8c7d9eb705933d258f8397f8fcef96
nnbenven <nnbenven@ncsu.edu>
2017-08-22 21:14:29 -0400

    Another small update to javadoc header

--- WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/Course.java
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/Course.java
@@ -1,13 +1,8 @@
-/**
- * 
- */
 package edu.ncsu.csc216.wolf_scheduler.course;
 
 /**
  * Object class that represents a course
- * 
  * @author Noah Benveniste
- *
  */
 public class Course {
     
