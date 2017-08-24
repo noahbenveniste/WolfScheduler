40834b4e6bd11b8be6abe1871d049dd8675ba3fe
nnbenven <nnbenven@ncsu.edu>
2017-08-23 22:46:05 -0400

    Fixed bug in CourseRecordIO where a course could be illegally
    constructed when the course meeting days was "A" and non zero times
    were listed

--- WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/io/CourseRecordIO.java
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/io/CourseRecordIO.java
@@ -124,7 +124,7 @@ public class CourseRecordIO {
     	//Once all tokens have been read in, try to create the object
     	try {
     		//If the meeting days is A, call the constructor for an arranged course
-    		if (meetingDays.equals("A")) {
+    		if (meetingDays.equals("A") && startTime == 0 && endTime == 0) {
     			c = new Course(courseName, courseTitle, sectionNum, credits, id, meetingDays);
     		} else {
     			c = new Course(courseName, courseTitle, sectionNum, credits, id, meetingDays, startTime, endTime);
