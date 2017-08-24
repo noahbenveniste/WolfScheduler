96dceae97e6c8d57e35f548c2c52eec412f8da96
nnbenven <nnbenven@ncsu.edu>
2017-08-23 23:25:24 -0400

    Added skeletons for WolfScheduler and WolfSchedulerTest

---
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/scheduler/WolfScheduler.java
@@ -0,0 +1,73 @@
+package edu.ncsu.csc216.wolf_scheduler.scheduler;
+
+import java.util.ArrayList;
+import edu.ncsu.csc216.wolf_scheduler.course.Course;
+
+public class WolfScheduler {
+	
+	/** Fields */
+	
+	/** Catalog of courses to be chosen from */
+	private ArrayList<Course> courseCatalog;
+	/** The student's schedule */
+	private ArrayList<Course> schedule;
+	/** The title of the schedule */
+	private String title;
+	
+	/** Constructor */
+	
+	public WolfScheduler(String invalidTestFile) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/** Methods */
+	
+	public Object getCourseFromCatalog(String string, String string2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public boolean addCourse(String string, String string2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public boolean removeCourse(String name, String section) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public boolean resetSchedule( ) {
+		
+	}
+	
+	public Object getCourseCatalog() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getScheduledCourses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getFullScheduledCourses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public void setTitle(Object object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void exportSchedule(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+}
